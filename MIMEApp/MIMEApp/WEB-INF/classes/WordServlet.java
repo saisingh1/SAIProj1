38d79d5e60a7e40b8cc3aec95ac4abbb6db36706
NILESHPC <NILESHPC@DESKTOP-2C22RSR>
2019-03-31 02:53:06 -0700

    First Commit

---
+++ MIMEApp/MIMEApp/WEB-INF/classes/WordServlet.java
@@ -0,0 +1,29 @@
+//WordServlet.java
+package com.nt.Servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class WordServlet extends HttpServlet
+{
+	public void service(HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException
+	{
+		PrintWriter pw=null;
+		//get PrintWriter object
+		pw=res.getWriter();
+		//set Response content type
+		res.setContentType("application/msword");
+		//write logic to generate output(webpage)
+		pw.println("<table border='1' bgcolor='cyan'>");
+		pw.println("<tr><td>Name</th><th>Surname<th></tr>");
+		pw.println("<tr><td>Asaram</th><th>Rathod<th></tr>");
+		pw.println("<tr><td>Godawari</th><th>Rathod<th></tr>");
+		pw.println("<tr><td>Ashwini</th><th>Rathod<th></tr>");
+		pw.println("<tr><td>Arati</th><th>Rathod<th></tr>");
+		pw.println("<tr><td>Amol</th><th>Rathod<th></tr>");
+		pw.println("</table>");
+		//close Stream
+		pw.close();
+	}//service(-,-)
+}//class
---
+++ MIMEApp/MIMEApp/WEB-INF/classes/XmlServlet.java
@@ -0,0 +1,32 @@
+//XmlServlet.java
+package com.nt.Servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class XmlServlet extends HttpServlet
+{
+	public void service(HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException
+	{
+		PrintWriter pw=null;
+		//get PrintWriter object
+		pw=res.getWriter();
+		//set Response content type
+		res.setContentType("text/xml");
+		//write logic to generate output(webpage)
+		pw.println("<table border='1' bgcolor='cyan'>");
+		pw.println("<tr><td>Name</th><th>Surname<th></tr>");
+		pw.println("<tr><td>Asaram</th><th>Rathod<th></tr>");
+		pw.println("<tr><td>Godawari</th><th>Rathod<th></tr>");
+		pw.println("<tr><td>Ashwini</th><th>Rathod<th></tr>");
+		pw.println("<tr><td>Arati</th><th>Rathod<th></tr>");
+		pw.println("<tr><td>Amol</th><th>Rathod<th></tr>");
+		pw.println("</table>");
+
+		res.setHeader("refresh","5");
+		
+		//close Stream
+		pw.close();
+	}//service(-,-)
+}//class
